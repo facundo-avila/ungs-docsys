705944a955d85b205b2e11c8872475328bd3768b
pabloabregu <pablo.abregu@hotmail.com>
2025-06-29 02:40:08 -0300

    Password recovery service added to bff

--- ungs-docsys-bff/src/modules/login/login.module.ts
+++ ungs-docsys-bff/src/modules/login/login.module.ts
@@ -4,11 +4,13 @@ import { AppUserService } from './app-user/services/app-user.service';
 import { AppUserController } from './app-user/controllers/app-user.controller';
 import { UserInfoController } from './user-info/controllers/user-info.controller';
 import { UserInfoService } from './user-info/services/user-info.service';
+import { PasswordResetController } from './password-reset/controllers/password-reset.controller';
+import { PasswordResetService } from './password-reset/services/password-reset.service';
 
 @Module({
-    controllers: [AppUserController, UserInfoController],
+    controllers: [AppUserController, UserInfoController, PasswordResetController],
     imports: [HttpModule],
-    providers: [AppUserService, UserInfoService],
-    exports: [AppUserService, UserInfoService]
+    providers: [AppUserService, UserInfoService, PasswordResetService],
+    exports: [AppUserService, UserInfoService, PasswordResetService]
 })
 export class LoginModule {}
